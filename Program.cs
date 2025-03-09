7ce4c98e5a240abf42548ea252b155b2eccb1442
tuan <tuan.nguyen@datics-consulting.com>
2025-03-09 14:49:34 +0000

    Initial commit: Added HertzDataAPI source code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using _50HertzDataAPI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Load Cosmos DB configuration
+var cosmosConfig = builder.Configuration.GetSection("CosmosDb");
+
+// Register MongoDB Client
+builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(cosmosConfig["ConnectionString"]));
+
+// Register MongoDbService
+builder.Services.AddSingleton<MongoDbService>();
+
+var app = builder.Build();
+app.MapControllers();
+app.Run();
